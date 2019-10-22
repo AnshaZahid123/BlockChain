28396d243e5e263ce02b42a24ae041247e7b719f
AnshaZahid123 <56825475+AnshaZahid123@users.noreply.github.com>
2019-10-22 11:24:56 +0500

    Add files via upload

---
+++ Block.cpp
@@ -0,0 +1,17 @@
+/*#include"Block.h"
+Block* Block::getNext() {
+    return this -> next;
+}
+
+std::string Block::getPreviousHash(void) {
+    return this -> previousHash;
+}
+
+std::string Block::getHash() {
+    return this -> Hash;
+}
+
+std::string Block::getData(){
+    return this -> data;
+}
+*/
